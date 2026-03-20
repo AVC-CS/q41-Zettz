cdf0c52052c2a24d95aa58e3939eeeec1960ad32
Lorenzo <calderonlorenzo026@gmail.com>
2026-03-20 00:13:24 +0000

    q41 code complete

--- main.cpp
+++ main.cpp
@@ -19,6 +19,33 @@ int main()
     cin >> choice;
 
     // TODO
+    switch (choice)
+    {    case 1:
+        cout << "Enter the radius of the circle: ";
+        cin >> radius;
+        area = PI * pow(radius, 2);
+        break;
+    case 2:
+        cout << "Enter the width of the rectangle: ";
+        cin >> width;
+        cout << "Enter the height of the rectangle: ";
+        cin >> height;
+        area = width * height;
+        break;
+    case 3:
+        cout << "Enter the base of the triangle: ";
+        cin >> base;
+        cout << "Enter the height of the triangle: ";
+        cin >> height;
+        area = 0.5 * base * height;
+        break;
+    case 4:
+        cout << "Program Stopped" << endl;
+        return 0;
+    default:
+        cout << "Wrong Number" << endl;
+        return 0;
+    }
 
     cout << left << setprecision(2) << fixed;
     cout << "The area is " << area << endl;
